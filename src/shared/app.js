ef284758b697d5815a16e9668b51514715182146
Matthew Quinn <iam@matthewquinn.me>
2016-08-11 16:41:07 +1200

    Moved error handling to utility lib to own util file

--- src/shared/app.js
+++ src/shared/app.js
@@ -4,51 +4,17 @@ import Menu from './components/navigation/menu';
 import Footer from './components/navigation/footer';
 import Header from './components/navigation/header';
 
+import errorHandling from './utils/errorHandling';
+
 // This is the controller view
 class App extends React.Component {
 
   componentWillMount() {
     if(typeof window !== 'undefined'){
-      window.onerror = this.errorHandling;
+      window.onerror = errorHandling;
     }
   }
 
-  errorHandling(message, file, line, column, errorObject) {
-    column = column || (window.event && window.event.errorCharacter);
-    let stack = errorObject ? errorObject.stack : null;
-
-    if(!stack) {
-        let stack = [];
-        let f = arguments.callee.caller;
-        while (f)
-        {
-            stack.push(f.name);
-            f = f.caller;
-        }
-        errorObject['stack'] = stack;
-    }
-
-    let data = {
-        message:message,
-        file:file,
-        line:line,
-        column:column,
-        errorStack:stack
-    };
-
-    console.error('----------------------');
-    console.error('ERROR CAUGHT IN APP.JS');
-    console.error('----------------------');
-    console.error(`MSG: ${data.message}`);
-    console.error(`IN: ${data.file}#${data.line}:${data.column}`);
-    console.error(data.errorStack)
-    console.error('--------------------');
-    console.error('Original Error Below');
-    console.error('--------------------');
-
-    return false;
-
-  }
 
   render() {
     return (
