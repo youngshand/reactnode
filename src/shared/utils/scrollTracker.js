e249c1f289fa18ce60bfe9761bb0902057e23fc3
Matthew Quinn <iam@matthewquinn.me>
2016-08-18 10:13:29 +1200

    Disable until we can refactor

--- src/shared/utils/scrollTracker.js
+++ src/shared/utils/scrollTracker.js
@@ -1,63 +1,63 @@
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 
-class ScrollTracker {
+// class ScrollTracker {
 
-	constructor () {
-		this.element = null;
-		this.target = null;
-		this.scrollInt = null;
-	}
+// 	constructor () {
+// 		this.element = null;
+// 		this.target = null;
+// 		this.scrollInt = null;
+// 	}
 
-	init(component, element, target) {
+// 	init(component, element, target) {
 
-		this.element = ReactDOM.findDOMNode(component.refs[element]);
-		this.target = ReactDOM.findDOMNode(component.refs[target]);
+// 		this.element = ReactDOM.findDOMNode(component.refs[element]);
+// 		this.target = ReactDOM.findDOMNode(component.refs[target]);
 
-		this.trackScrollPos(this.element, this.target);
-	}
+// 		this.trackScrollPos(this.element, this.target);
+// 	}
 
-	componentWillUnmount() {
-		clearInterval(this.scrollInt);
-		this.scrollInt = null;
-	}
+// 	componentWillUnmount() {
+// 		clearInterval(this.scrollInt);
+// 		this.scrollInt = null;
+// 	}
 
-	trackScrollPos(element, target) {
-		// const buffer = 200;
-		const targetHeight = target.offsetHeight;
+// 	trackScrollPos(element, target) {
+// 		// const buffer = 200;
+// 		const targetHeight = target.offsetHeight;
 
-		// let elementTop = element.getBoundingClientRect().top;
-		// let windowTop = window.pageYOffset;
-		// let elementBottom = element.getBoundingClientRect().bottom;
+// 		// let elementTop = element.getBoundingClientRect().top;
+// 		// let windowTop = window.pageYOffset;
+// 		// let elementBottom = element.getBoundingClientRect().bottom;
 
-		this.scrollInt = setInterval(() => {
-			let elementTop = element.getBoundingClientRect().top;
-			// let windowTop = window.pageYOffset;
-			let elementBottom = element.getBoundingClientRect().bottom;
+// 		this.scrollInt = setInterval(() => {
+// 			let elementTop = element.getBoundingClientRect().top;
+// 			// let windowTop = window.pageYOffset;
+// 			let elementBottom = element.getBoundingClientRect().bottom;
 
-			// if (elementTop > (buffer / 2)) {
-			//   // set pos to relative
-			// } else if (elementTop < -(buffer / 2)) {
-			//   // pos should be fixed
-			// } else {
-			//   const top = windowTop / buffer;
-			//   // this.setState({ imageTop: top });
-			// }
+// 			// if (elementTop > (buffer / 2)) {
+// 			//   // set pos to relative
+// 			// } else if (elementTop < -(buffer / 2)) {
+// 			//   // pos should be fixed
+// 			// } else {
+// 			//   const top = windowTop / buffer;
+// 			//   // this.setState({ imageTop: top });
+// 			// }
 
-			if (elementTop <= 0) {
-				target.setAttribute('data-fixed', 'true');
-			} else {
-				target.removeAttribute('data-fixed');
-			}
+// 			if (elementTop <= 0) {
+// 				target.setAttribute('data-fixed', 'true');
+// 			} else {
+// 				target.removeAttribute('data-fixed');
+// 			}
 
-			if (elementBottom <= targetHeight) {
-				target.setAttribute('data-absolute', 'true');
-			} else {
-				target.removeAttribute('data-absolute');
-			}
-		}, 1);
+// 			if (elementBottom <= targetHeight) {
+// 				target.setAttribute('data-absolute', 'true');
+// 			} else {
+// 				target.removeAttribute('data-absolute');
+// 			}
+// 		}, 1);
 
-	}
+// 	}
 
-}
+// }
 
-export default new ScrollTracker;
+// export default new ScrollTracker;
