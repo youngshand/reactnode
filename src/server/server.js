6af0b2aed8b0c90863ebfbc48b7bbf98bef0b094
Matthew Quinn <iam@matthewquinn.me>
2016-08-18 11:12:05 +1200

    Dont import browsersync in production

--- src/server/server.js
+++ src/server/server.js
@@ -1,19 +1,20 @@
 import app from './app';
 import { PORT, LOCALHOST, ENV } from './config';
-import browserSync from 'browser-sync';
 import updateAPI from './updateAPI';
-
-browserSync.use({
-    plugin: function () { /* noop */},
-    hooks: {
-        'client:js': require('fs').readFileSync(__dirname + '/reloader.js', 'utf-8')
-    }
-});
 
 app.listen(PORT, () => {
 
   // Load up browserSync if we're on a local dev environment
   if (ENV === 'local') {
+    const browserSync = require('browser-sync');
+
+    browserSync.use({
+        plugin: function () { /* noop */},
+        hooks: {
+            'client:js': require('fs').readFileSync(__dirname + '/reloader.js', 'utf-8')
+        }
+    });
+
     browserSync({
       proxy: 'localhost:' + PORT,
       files: {
@@ -23,9 +24,9 @@ app.listen(PORT, () => {
     });
   }
 
+
   console.log(`Listening on ${LOCALHOST}`);
 
   // Fire off an API update on server start
   updateAPI();
-
 });
