15ef4fe0c383ddf2758ccc8fadc31cdbc49fc980
Matthew Quinn <iam@matthewquinn.me>
2016-08-18 10:12:48 +1200

    Formatting

--- src/shared/components/common/imageWrap.js
+++ src/shared/components/common/imageWrap.js
@@ -1,40 +1,36 @@
-import React from 'react';
-import ImageHandler from '../../utils/imageHandler';
+import React, { PropTypes } from 'react';
+// import ImageHandler from '../../utils/imageHandler';
 
 class ImageWrap extends React.Component {
 
-  static propTypes = {
-    src: React.PropTypes.string.isRequired,
-    altText: React.PropTypes.string.isRequired,
-    landscape: React.PropTypes.string.isRequired,
-    portrait: React.PropTypes.string.isRequired,
-    desktop: React.PropTypes.string.isRequired,
-    classes: React.PropTypes.string.isRequired
-  }
+	componentDidMount() {
+		// ImageHandler.init(this);
+	}
 
-  componentDidMount() {
-    ImageHandler.init(this);
-  }
+	render() {
+		const src = this.props.src;
+		const alt = this.props.altText;
+		const classes = this.props.classes || '';
+		const landscape = this.props.landscape ? this.props.landscape : '';
+		const portrait = this.props.portrait ? this.props.portrait : '';
+		const desktop = this.props.desktop ? this.props.desktop : '';
 
-  render() {
+		return (
+			<div className={`image-wrap ${classes}`} data-desktop={desktop} data-landscape={landscape} data-portrait={portrait}>
+				<img src={src} alt={alt} />
+			</div>
+		);
+	}
 
-    let src = this.props.src,
-        alt = this.props.altText,
-        classes = '',
-        landscape = this.props.landscape ? this.props.landscape : '',
-        portrait = this.props.portrait ? this.props.portrait : '',
-        desktop = this.props.desktop ? this.props.desktop : '';
+}
 
-    if (this.props.classes) {
-      classes = this.props.classes;
-    }
-
-    return (
-      <div className={`image-wrap ${classes}`} data-desktop={desktop} data-landscape={landscape} data-portrait={portrait}>
-        <img src={src} alt={alt} />
-      </div>
-    );
-  }
+ImageWrap.propTypes = {
+	src: PropTypes.string.isRequired,
+	altText: PropTypes.string.isRequired,
+	landscape: PropTypes.string.isRequired,
+	portrait: PropTypes.string.isRequired,
+	desktop: PropTypes.string.isRequired,
+	classes: PropTypes.string.isRequired
 }
 
 export default ImageWrap;
