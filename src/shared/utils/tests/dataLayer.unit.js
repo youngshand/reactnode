2f7d3ac1cb76ba9dde576b21e44dab00206321a1
David Featherston <david@lambry.com>
2016-09-22 11:03:15 +1200

    Add global jsdom for testing

--- src/shared/utils/tests/dataLayer.unit.js
+++ src/shared/utils/tests/dataLayer.unit.js
@@ -1,6 +1,6 @@
 import { assert } from 'chai';
-import { jsdom } from 'jsdom';
 import { pageView } from '../dataLayer';
+require('jsdom-global')();
 
 describe('dataLayer', () => {
 
@@ -8,9 +8,7 @@ describe('dataLayer', () => {
    * Test virtualPageview function
    */
   it('should push a pageView to the dataLayer', () => {
-    global.document = jsdom('');
-    global.window = document.defaultView;
-    global.window.dataLayer = [];
+    window.dataLayer = [];
 
     pageView('http://test.dev/page');
     assert.propertyVal(window.dataLayer[0], 'virtualUrl', 'http://test.dev/page');
